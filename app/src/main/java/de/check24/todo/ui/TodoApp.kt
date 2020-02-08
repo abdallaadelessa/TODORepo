6b58de691ccd922951462f2c9155b122e9e4093e
Abdullah Essa <Abdullah.Essa@check24.de>
2020-02-08 12:40:00 +0100

    Fix concurrent modification exception

--- app/src/main/java/de/check24/todo/ui/TodoApp.kt
+++ app/src/main/java/de/check24/todo/ui/TodoApp.kt
@@ -1,18 +1,19 @@
 package de.check24.todo.ui
 
 import androidx.compose.*
-import androidx.ui.animation.Crossfade
 import androidx.ui.material.DrawerState
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-
 import de.check24.todo.pojo.SettingsState
 import de.check24.todo.pojo.Todo
 import de.check24.todo.ui.screens.*
 
 @Model
 object TodoApp {
+
     var currentScreen: Screen = Screen.Overview
+        private set
+
     val currentDrawerState: State<DrawerState> = +state { DrawerState.Closed }
 
     fun navigateTo(destination: Screen) {
@@ -32,18 +33,16 @@ object TodoApp {
 @Composable
 fun TodoApp(settingsState: SettingsState) {
     MaterialTheme(colors = getColorPalette(settingsState.darkMode)) {
-        Crossfade(TodoApp.currentScreen) { screen ->
-            when (screen) {
+        when (TodoApp.currentScreen) {
 
-                is TodoApp.Screen.Login -> LoginAppScreen(userInfo = UserInfo())
-
-                else -> MainContent(TodoApp.currentDrawerState) {
-                    when (screen) {
-                        is TodoApp.Screen.Overview -> OverviewScreen()
-                        is TodoApp.Screen.Create -> CreateScreen()
-                        is TodoApp.Screen.Details -> DetailsScreen()
-                        is TodoApp.Screen.Settings -> SettingsScreen(settingsState)
-                    }
+            is TodoApp.Screen.Login -> LoginAppScreen(userInfo = UserInfo())
+                
+            else -> MainContent(TodoApp.currentDrawerState) {
+                when (TodoApp.currentScreen) {
+                    is TodoApp.Screen.Overview -> OverviewScreen()
+                    is TodoApp.Screen.Create -> CreateScreen()
+                    is TodoApp.Screen.Details -> DetailsScreen()
+                    is TodoApp.Screen.Settings -> SettingsScreen(settingsState)
                 }
             }
         }
